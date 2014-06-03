2878584d0ab37f192cc3cc6b9f8caed97de0054e
lsadam0 <lsadam0gmail.com>
2014-06-03 19:52:30 -0400

    Missing FIles

---
+++ BEx/CryptoCurrency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BEx
+{
+    public enum CryptoCurrency
+    {
+        BitCoin = 0, // BTC
+        LiteCoin = 1, // LTC
+        NameCoin = 2, // NMC
+        NovaCoin = 3, // NVC
+        PeerCoin = 4, // PPC
+        TerraCoin = 5, // TRC
+        FeatherCoin = 6, // FTC
+        PrimeCoin = 7 // XPM
+
+    }
+}
