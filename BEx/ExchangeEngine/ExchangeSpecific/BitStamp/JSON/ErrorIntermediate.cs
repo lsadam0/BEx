89a15bca8776a328e4d8d13be7ae05dd934d6c7f
lsadam0 <lsadam0@gmail.com>
2015-04-08 08:43:06 -0400

    Variant Bitstamp Error JSON

--- BEx/ExchangeEngine/ExchangeSpecific/BitStamp/JSON/ErrorIntermediate.cs
+++ BEx/ExchangeEngine/ExchangeSpecific/BitStamp/JSON/ErrorIntermediate.cs
@@ -8,7 +8,8 @@ namespace BEx.ExchangeEngine.BitStamp.JSON
 {
     internal class ErrorIntermediate : IExchangeResponse
     {
-       
+
+        [JsonConverter(typeof(SingleValueArrayConverter))]
         public Error error { get; set; }
 
         public class Error
@@ -29,9 +30,47 @@ namespace BEx.ExchangeEngine.BitStamp.JSON
                 Message = sb.ToString()
             };
         }
+
+
+
+        public class SingleValueArrayConverter : JsonConverter
+        {
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                Error res = null;
+
+
+                if (reader.TokenType == JsonToken.String)
+                {
+                    string message = reader.Value.ToString();
+
+                    string[] all = new string[1];
+                    all[0] = message;
+
+                    res = new Error()
+                    {
+                        __all__ = all
+                    };
+                }
+                else if (reader.TokenType == JsonToken.StartObject)
+                {
+                    res = serializer.Deserialize<Error>(reader);
+
+                }
+
+                return res;
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return false;
+            }
+        }
     }
-
-    
-
-
 }
+
